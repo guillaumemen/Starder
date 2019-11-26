6d7333320c2ba4d00d496c5806dd79418cb7f12e
guillaumemen <guillaume.menard77@icloud.com>
2019-11-26 12:44:33 +0100

    add searchpage

---
+++ starder/resources/views/searchpage.blade.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Search</title>
+
+</head>
+ <body>
+   <div class="container">
+    <form action="/search" method="POST" role="search">
+        {{ csrf_field() }}
+        <div class="input-group">
+            <input type="text" class="form-control" name="q"
+                   placeholder="Search users"> <span class="input-group-btn">
+					<button type="submit" class="btn btn-default">
+						<span class="glyphicon glyphicon-search"></span>
+					</button>
+				</span>
+        </div>
+    </form>
+    <div class="container">
+        @if(isset($details))
+            <p> The Search results for your query <b> {{ $query }} </b> are :</p>
+            <h2>Sample User details</h2>
+            <table class="table table-striped">
+                <thead>
+                <tr>
+                    <th>Name</th>
+                    <th>Email</th>
+                </tr>
+                </thead>
+                <tbody>
+                @foreach($details as $user)
+                    <tr>
+                        <td>{{$user->name}}</td>
+                        <td>{{$user->email}}</td>
+                    </tr>
+                @endforeach
+                </tbody>
+            </table>
+        @elseif(isset($message))
+            <p>{{ $message }}</p>
+        @endif
+    </div>
+
+ </body>
+</html>
